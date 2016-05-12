e5b46bc8ebb026163ad9417fdf451865cabaf00a
Zachary Olsen <zolsen1@owlmail.harford.edu>
2016-05-12 00:29:26 -0400

    isotope masonry js

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
 		$(".fancybox").fancybox();
 	});
+
+// external js: isotope.pkgd.min.js
+
+	$('.grid').isotope({
+	  itemSelector: '.grid-item',
+	  masonry: {
+	    columnWidth: '.grid-sizer'
+	  }
+	});
